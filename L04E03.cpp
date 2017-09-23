8b17cc1a4ca5e51f83fb0424b20745715594145b
Adriel Araujo <araujo.contato021@gmail.com>
2017-09-23 17:20:12 -0300

    Add files via upload

---
+++ L04E03.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void junta( int v1[], int v2[] , int n1, int n2);
+
+int main(){
+
+	int *v1;
+	int *v2;
+	int i, n1, n2;
+
+	printf("Digite a quantidade de elementos do primeiro vetor: \n");
+	scanf("%d", &n1);
+	v1= malloc(n1*sizeof(int));
+
+	printf("Digite a quantidade de elementos do segundo vetor: \n");
+	scanf("%d", &n2);
+	v2= malloc(n2*sizeof(int));
+	
+	printf("\n\n");
+
+	for(i=0; i<n1; i++){
+		printf("Digite o valor da posicao %d do primeiro vetor: ", i+1);
+		scanf("%d", &v1[i]);
+
+	}
+
+	printf("\n\n");
+	
+	for(i=0; i<n1; i++){
+		printf("Digite o valor da posicao %d do segundo vetor: ", i+1);
+		scanf("%d", &v2[i]);
+
+	}
+
+	junta(v1, v2, n1, n2);
+
+return 0;
+}
+
+void junta( int v1[], int v2[] , int n1, int n2){
+    int i, n3, *v3;
+    int j=0;
+    n3= n1+n2;
+	v3= malloc(n3*sizeof(int));
+
+	for(i=0; i<n1; i++){
+		v3[i]=v1[i];
+	}
+
+	for(i=n1; i<n3; i++){
+		while( j<j+1){
+			v3[i]=v2[j];
+        j++;
+		}
+	}
+
+	for(i=0; i<n3; i++){
+
+	printf("%d", v3[i]);
+
+	}
+}
