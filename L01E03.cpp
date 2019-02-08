99f69cf9a95b3c87077878b79c1276acdce184d7
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-07 23:34:44 -0300

    Update L01E03.cpp

--- L01E03.cpp
+++ L01E03.cpp
@@ -1,9 +1,14 @@
-/*Questão 3. Escreva um programa que lê uma string de até 50 caracteres, e imprime “Palindromo”
+/*Questão 3: Escreva um programa que lê uma string de até 50 caracteres, e imprime “Palindromo”
 (uma palavra ou frase, que é igual quando lida da esquerda para a direita ou da direita para a
 esquerda) caso a string seja um palindromo e “Nao Palindromo”caso contrário. Exemplo de
 palindromo: saudavel leva duas.
-Autor: Adriel Araujo - Estudante de Ciencias da Computação pela UECE - 1422340
-*/
+Question 3: Write a program that reads a string of up to 50 characters, and prints "Palindromo" 
+(a word or phrase, which is the same when read from left to right or right to left) if the string
+is a palindrome and " "otherwise. Example of palindromo: healthy takes two.
+
+Autor/Author: Adriel Araujo
+-Estudante de Ciencias da Computação UECE 2016.2 - 1422340
+-Computer Science student in Ceara State University*/
 
 #include <stdio.h>
 #include <string.h>
