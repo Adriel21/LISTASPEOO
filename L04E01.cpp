23eae2c26fb60ced29f21c0a5aef90021d4e9194
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-07 23:37:59 -0300

    Update L04E01.cpp

--- L04E01.cpp
+++ L04E01.cpp
@@ -10,8 +10,10 @@ struct Pessoa cadastro[100];
 Suponha que o vetor esteja ordenado em ordem crescente por valor de RG. Implemente uma função
 de busca por RG, que opera como a busca binária, e que caso exista uma pessoa no cadastro com o
 RG a ser buscado, devolve o índice deste cadastro e caso não exista o RG a ser buscado, devolve -1.
-Autor: Adriel Araujo
-- Estudante de Ciencias da Computação pela UECE 2016.2 - 1422340*/
+
+Autor/Author: Adriel Araujo
+-Estudante de Ciencias da Computação UECE 2016.2 - 1422340
+-Computer Science student in Ceara State University*/
 
 #include <stdio.h>
 
